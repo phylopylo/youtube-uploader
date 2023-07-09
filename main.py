0e6e72d4ac8aabfa2e76db99d0b102bf5b1d7f6f
Justin <69047638+Justin-Meme-Sometimes@users.noreply.github.com>
2023-07-08 23:09:17 -0400

    added some an input that may automatically allow accounts to load

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ import time
 
 videoInfo = VideoMetadata()
 
-videoInfo.video_path = r"C:\Users\Philip\Documents\programming\youtube-uploader\video-samples\good_job.mp4"
+videoInfo.video_path = r"C:\Users\justi\OneDrive\Documents\GitHub\youtube-uploader\video-samples\good_job.mp4"
 videoInfo.video_title = "My Honest Reaction"
 videoInfo.video_description = "Well done."
 videoInfo.video_tags = "Memes, Soylent, Internet, Redditor"
+videoInfo.video_thumbnail_path = r"C:\Users\justi\OneDrive\Documents\GitHub\youtube-uploader\video-samples\gotta catch em all.jpg"
 
 automation = YoutubeUploader()
-# automation.upload_video(videoInfo)
+#automation.setup_driver()
+automation.upload_video(videoInfo)
+
 while True:
     time.sleep(1)
