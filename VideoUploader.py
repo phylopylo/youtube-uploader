58d634f100081fca41a132e4650f81ef459f29ae
Philip Roberts <philip.r.roberts@gmail.com>
2023-07-14 19:57:15 -0400

    Updated file headers and license

--- VideoUploader.py
+++ VideoUploader.py
@@ -1,9 +1,27 @@
 """
 YouTube Video Uploader Tool
-
 Authors: Philip Roberts and Justin Bell
 
-This software is licensed under the LGPL. View the license here: https://www.gnu.org/licenses/lgpl-3.0.en.html
+======================================================================
+
+                    YouTube Video Uploader
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+    USA
+
+======================================================================
 """
 
 from selenium import webdriver
